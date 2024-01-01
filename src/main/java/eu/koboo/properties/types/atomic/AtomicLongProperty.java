a37628c36c23857648e9304878d76609ca933bdc
Koboo <admin@koboo.eu>
2024-01-01 21:43:58 +0100

    Add AtomicLongProperty

---
+++ src/main/java/eu/koboo/properties/types/atomic/AtomicLongProperty.java
@@ -0,0 +1,13 @@
+package eu.koboo.properties.types.atomic;
+
+import eu.koboo.properties.VarProperty;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class AtomicLongProperty extends VarProperty<AtomicLong> {
+
+    public AtomicLongProperty(String propertyKey, AtomicLong defaultValue) {
+        super(propertyKey, defaultValue, AtomicLong.class, string -> new AtomicLong(Long.parseLong(string)));
+    }
+}
